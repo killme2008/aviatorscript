b0720d005962118ed11ca7af16bc32e55511eee6
dennis <dennis@dennis-laptop.(none)>
2011-05-18 21:02:52 +0800

    ignore script engine test

--- src/test/java/com/googlecode/aviator/script/AviatorEngineUnitTest.java
+++ src/test/java/com/googlecode/aviator/script/AviatorEngineUnitTest.java
@@ -1,20 +1,15 @@
 package com.googlecode.aviator.script;
 
-import static org.junit.Assert.*;
-
-import javax.script.ScriptEngineManager;
-
-import org.junit.Ignore;
 
 
 public class AviatorEngineUnitTest {
-    private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager(Thread.currentThread().getContextClassLoader());
-
-
-    @Ignore
-    public void getScriptEngineByName() {
-       
-        AviatorScriptEngine engine = (AviatorScriptEngine)this.scriptEngineManager.getEngineByName("Aviator");
-        assertNotNull(engine);
-    }
+//    private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager(Thread.currentThread().getContextClassLoader());
+//
+//
+//    @Ignore
+//    public void getScriptEngineByName() {
+//       
+//        AviatorScriptEngine engine = (AviatorScriptEngine)this.scriptEngineManager.getEngineByName("Aviator");
+//        assertNotNull(engine);
+//    }
 }
