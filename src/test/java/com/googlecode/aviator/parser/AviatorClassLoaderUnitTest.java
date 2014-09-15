e335bf1b5e17ebc5e549b9cecc4e73c243d4db3b
dennis zhuang <killme2008@gmail.com>
2014-09-15 18:12:18 +0800

    Fixed memory leak when doesn't using cache.

---
+++ src/test/java/com/googlecode/aviator/parser/AviatorClassLoaderUnitTest.java
@@ -0,0 +1,21 @@
+package com.googlecode.aviator.parser;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import com.googlecode.aviator.AviatorEvaluator;
+
+
+public class AviatorClassLoaderUnitTest {
+
+    @Test
+    public void testManyManyExpressions() {
+        for (int i = 0; i < 10000; i++) {
+            HashMap<String, Object> env = new HashMap<String, Object>();
+            env.put("a" + i, i);
+            env.put("b" + i, i-1);
+            AviatorEvaluator.execute("a" + i + ">b" + i, env);
+        }
+    }
+}
