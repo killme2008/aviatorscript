fd292fb2e7989420b53e1dda4927a4fd6206571f
dennis zhuang <killme2008@gmail.com>
2016-10-09 11:22:19 +0800

    (fix) Revert simple example

--- src/test/java/com/googlecode/aviator/example/SimpleExample.java
+++ src/test/java/com/googlecode/aviator/example/SimpleExample.java
@@ -5,7 +5,7 @@ import com.googlecode.aviator.AviatorEvaluator;
 
 public class SimpleExample {
     public static void main(String[] args) {
-        Long result = (Long) AviatorEvaluator.execute("4( * ssss”");
+        Long result = (Long) AviatorEvaluator.execute("1+2+3");
         System.out.println(result);
     }
 }
