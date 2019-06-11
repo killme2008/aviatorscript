8b535cfd58eb92f64b6d189bdb73710c7cd7f3fc
Jerry Lee <oldratlee@gmail.com>
2019-06-11 17:49:48 +0800

    fix typo: Sring -> Spring

--- src/main/java/com/googlecode/aviator/spring/SringContextFunctionLoader.java
+++ src/main/java/com/googlecode/aviator/spring/SringContextFunctionLoader.java
@@ -1,50 +1,24 @@
 package com.googlecode.aviator.spring;
 
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import com.googlecode.aviator.FunctionLoader;
-import com.googlecode.aviator.runtime.type.AviatorFunction;
 
 /**
  * Function loader based on spring context, try to find the function by name from spring context.
  *
  * @since 4.0.0
  * @author dennis
+ * @deprecated Use {@link SpringContextFunctionLoader} instead.
  *
  */
-public class SringContextFunctionLoader implements FunctionLoader {
-
-  private ApplicationContext applicationContext;
-
-
+@Deprecated
+public class SringContextFunctionLoader extends SpringContextFunctionLoader
+    implements FunctionLoader {
   public SringContextFunctionLoader() {
     super();
   }
 
-
   public SringContextFunctionLoader(ApplicationContext applicationContext) {
-    super();
-    this.applicationContext = applicationContext;
+    super(applicationContext);
   }
-
-
-  public ApplicationContext getApplicationContext() {
-    return applicationContext;
-  }
-
-
-  public void setApplicationContext(ApplicationContext applicationContext) {
-    this.applicationContext = applicationContext;
-  }
-
-
-  @Override
-  public AviatorFunction onFunctionNotFound(String name) {
-    try {
-      return (AviatorFunction) this.applicationContext.getBean(name);
-    } catch (NoSuchBeanDefinitionException e) {
-      return null;
-    }
-  }
-
 }
