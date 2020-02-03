2cdbcc304a4f11e8e2144bd2715a7d957a7e5158
dennis <killme2008@gmail.com>
2020-02-03 11:16:35 +0800

    (feat) Adds range

---
+++ src/main/java/com/googlecode/aviator/runtime/function/system/ReducerFunction.java
@@ -0,0 +1,12 @@
+package com.googlecode.aviator.runtime.function.system;
+
+import com.googlecode.aviator.runtime.function.AbstractFunction;
+
+public class ReducerFunction extends AbstractFunction {
+
+  @Override
+  public String getName() {
+    return "__reduce";
+  }
+
+}
---
+++ src/main/java/com/googlecode/aviator/runtime/type/Range.java
@@ -0,0 +1,92 @@
+package com.googlecode.aviator.runtime.type;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import com.googlecode.aviator.utils.Env;
+
+/**
+ * A range in [start, end] with step.
+ *
+ * @author dennis(killme2008@gmail.com)
+ * @since 5.0.0
+ */
+public final class Range extends AviatorObject implements Iterable<AviatorNumber> {
+  private static final AviatorLong ZERO = AviatorLong.valueOf(0L);
+  private final AviatorNumber step;
+  private final AviatorNumber start;
+  private final AviatorNumber end;
+
+
+  public Range(final AviatorNumber start, final AviatorNumber end, final AviatorNumber step) {
+    super();
+    this.start = start;
+    this.end = end;
+    this.step = step;
+  }
+
+  @Override
+  public String desc(final Map<String, Object> env) {
+    return "<Range, [" + this.start.getValue(env) + ", " + this.end.getValue(env) + "], "
+        + this.step.getValue(env) + ">";
+  }
+
+
+  @Override
+  public int compare(final AviatorObject other, final Map<String, Object> env) {
+    throw new UnsupportedOperationException();
+  }
+
+
+
+  @Override
+  public AviatorType getAviatorType() {
+    return AviatorType.Range;
+  }
+
+
+
+  @Override
+  public Object getValue(final Map<String, Object> env) {
+    return this;
+  }
+
+
+
+  public AviatorNumber first() {
+    return this.start;
+  }
+
+  public AviatorNumber last() {
+    return this.end;
+  }
+
+  @Override
+  public Iterator<AviatorNumber> iterator() {
+    return new Iterator<AviatorNumber>() {
+      AviatorNumber current = Range.this.start;
+
+      @Override
+      public boolean hasNext() {
+        if (Range.this.step.compare(ZERO, Env.EMPTY_ENV) >= 0) {
+          return this.current.compare(Range.this.end, Env.EMPTY_ENV) <= 0;
+        } else {
+          return this.current.compare(Range.this.end, Env.EMPTY_ENV) >= 0;
+        }
+      }
+
+      @Override
+      public AviatorNumber next() {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+        AviatorNumber result = this.current;
+        this.current = (AviatorNumber) this.current.add(Range.this.step, Env.EMPTY_ENV);
+        return result;
+      }
+
+    };
+  }
+
+
+}
