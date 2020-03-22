fc959c6362d49f843726c25732e9bee48ece24f7
dennis <killme2008@gmail.com>
2020-03-22 22:49:00 +0800

    (fix) revert SimpleExample

--- src/test/java/com/googlecode/aviator/example/SimpleExample.java
+++ src/test/java/com/googlecode/aviator/example/SimpleExample.java
@@ -1,16 +1,11 @@
 package com.googlecode.aviator.example;
 
-import java.util.HashMap;
-import java.util.Map;
 import com.googlecode.aviator.AviatorEvaluator;
 
 
 public class SimpleExample {
   public static void main(final String[] args) throws Exception {
-    Map<String, Object> env = new HashMap<String, Object>();
-    String result = (String) AviatorEvaluator
-        .execute("result=true;v1='test1'; if(!result) {return 'ok';} v2='test2';v2", env);
+    Long result = (Long) AviatorEvaluator.execute("1+2+3");
     System.out.println(result);
-    System.out.println(env);
   }
 }
