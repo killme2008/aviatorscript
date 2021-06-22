5707e5f923f5b8732da1c41bd56d3207c13e2e40
dennis <killme2008@gmail.com>
2021-06-22 22:17:50 +0800

    (fix) format code

--- src/main/java/com/googlecode/aviator/runtime/function/seq/SeqGetFunction.java
+++ src/main/java/com/googlecode/aviator/runtime/function/seq/SeqGetFunction.java
@@ -35,7 +35,8 @@ public class SeqGetFunction extends AbstractFunction {
     Object coll = arg1.getValue(env);
     Object key = arg2.getValue(env);
     if (coll == null) {
-      throw new NullPointerException("the collection of seq.get is null, which the value of key/index is `" + key + "`");
+      throw new NullPointerException(
+          "the collection of seq.get is null, which the value of key/index is `" + key + "`");
     }
     Class<?> clazz = coll.getClass();
 
